0f2b2bc6f934ca9bf2307e86a3c29ea72d079e4b
iago oliveira fonseca <iagogoddigital@hotmail.com>
2023-04-30 16:26:39 -0300

    Implementando o Scroll Suave

--- script.js
+++ script.js
@@ -5,3 +5,24 @@ $(document).ready(function(){
         $(".menu").toggleClass('active');
     });
 })
+
+const menuItems = document.querySelectorAll('.menu a[href^="#"]')
+
+menuItems.forEach(item => {
+    item.addEventListener('click', scrollToIdOnClick)
+})
+
+function scrollToIdOnClick(event){
+    event.preventDefault();
+    const element = event.target;
+    const id = element.getAttribute('href');
+    const to = document.querySelector(id).offsetTop;
+   
+
+
+    window.scroll({
+        top: to - 80,
+        behavior: "smooth"
+
+    });
+}
