ee88d08b0235a7540332269fabb68d5073a594b9
iago oliveira fonseca <iagogoddigital@hotmail.com>
2023-05-01 08:48:55 -0300

    resolvendo o bugzinho do menu

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+
 $(document).ready(function(){
     $('.hamburguer').click(function(){
         $(this).toggleClass('active');
@@ -6,23 +8,45 @@ $(document).ready(function(){
     });
 })
 
+
+/* 
+
+function toggleMenu(){
+    const menu = document.getElementsByClassName("menu")[0]
+    const hamburguer = document.getElementsByClassName("hamburguer")[0]
+    hamburguer.classList.toggle('active')
+    menu.classList.toggle('active')
+}
+
+*/
+
+
+
+function getScrollTopByHref(element){
+
+    const id = element.getAttribute('href');
+    return document.querySelector(id).offsetTop;
+}
+
 const menuItems = document.querySelectorAll('.menu a[href^="#"]')
 
 menuItems.forEach(item => {
     item.addEventListener('click', scrollToIdOnClick)
 })
 
+
 function scrollToIdOnClick(event){
     event.preventDefault();
-    const element = event.target;
-    const id = element.getAttribute('href');
-    const to = document.querySelector(id).offsetTop;
-   
+    const to = getScrollTopByHref(event.target) - 130
 
+    scrollToPosition(to);
+}
 
+function scrollToPosition(to){
     window.scroll({
-        top: to - 80,
+        top: to,
         behavior: "smooth"
 
     });
 }
+
